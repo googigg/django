e6dc5260e04fca9faf112965ea0675ea32fbf9ba
TISCOGROUP\Zsowat <zsowat@tisco.co.th>
2017-09-19 10:32:31 +0700

    remove hello.py

--- hello.py
+++ hello.py
@@ -4,12 +4,6 @@ from engine.core.db.mongo import MongoTestDrive
 from engine.core.util.http import HttpUtil
 
 print('This is a unit test.')
-#
-# t = Task()
-# t.run()
-
-# m = MongoTestDrive()
-# m.add()
 
 
 
